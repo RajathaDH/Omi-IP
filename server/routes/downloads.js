ffdbc20afac857c5c7bf039dabf078fadb6b3b09
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-05-30 07:28:18 +0530

    Added download routes and logo icon

--- server/routes/downloads.js
+++ server/routes/downloads.js
@@ -6,4 +6,12 @@ router.get('/omi/windows', (req, res) => {
     res.download('downloads/omi-windows.rar');
 });
 
+router.get('/omi/linux', (req, res) => {
+    res.download('downloads/omi-linux.rar');
+});
+
+router.get('/omi/mac', (req, res) => {
+    res.download('downloads/omi-mac.rar');
+});
+
 module.exports = router;
